709444c13a6689ad01fea316e07225580d4c1b84
Dawit Belay <dawitbelay0707@gmail.com>
2024-03-13 12:46:32 +0300

    first stateful widget build

--- first_new/lib/gradiant_container.dart
+++ first_new/lib/gradiant_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dice_roller.dart';
 // import './style_widget.dart';
 
 var beginAlign = Alignment.topRight;
@@ -6,22 +7,15 @@ var endAlign = Alignment.bottomLeft;
 
 class GradaintContainer extends StatelessWidget {
   //  required this.colors
-  GradaintContainer(this.color1, this.color2, {super.key});
+  const GradaintContainer(this.color1, this.color2, {super.key});
 
-  GradaintContainer.purple({super.key})
+  const GradaintContainer.purple({super.key})
       : color1 = Colors.blue,
         color2 = Colors.greenAccent;
 
   // final List<Color> colors;
   final Color color1;
   final Color color2;
-
-  var activeImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeImage = 'assets/images/dice-2.png';
-    print("image is ");
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,33 +37,11 @@ class GradaintContainer extends StatelessWidget {
           // Color.fromARGB(95, 255, 4, 4),
         ], end: endAlign, begin: beginAlign),
       ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            activeImage,
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              // padding: const EdgeInsets.only(top: 20),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(
-                fontSize: 30,
-              ),
-            ),
-            child: const Text("Roll dices"),
-          )
-        ],
-      )
-          //Elveted button,outlined button, textButton
-          // StyleWidget("Hello world", 70, Colors.deepOrangeAccent)
-          ),
+      child: const Center(
+        child: DiceRoller(),
+        //Elveted button,outlined button, textButton
+        // StyleWidget("Hello world", 70, Colors.deepOrangeAccent)
+      ),
     );
   }
 }
